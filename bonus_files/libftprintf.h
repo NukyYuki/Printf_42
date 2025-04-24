10f872c3133e387f6229703ac716eabb9eec16c7
Manuel Maria Carreiro Couto <manmaria@c1r10s5.42porto.com>
2025-04-24 22:15:04 +0100

    disvster: ft_flags wip, cpyed .h to bonus_files/

--- bonus_files/libftprintf.h
+++ bonus_files/libftprintf.h
@@ -3,17 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   libftprintf.h                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: manmaria <manmaria@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: mipinhei <mipinhei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/22 17:50:48 by manmaria          #+#    #+#             */
-/*   Updated: 2025/04/22 18:21:12 by manmaria         ###   ########.fr       */
+/*   Created: 2025/04/22 13:22:25 by mipinhei          #+#    #+#             */
+/*   Updated: 2025/04/24 20:48:20 by manmaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFTPRINTF_H
 # define LIBFTPRINTF_H
 
-# include <stdlib.h>
+# include <stdarg.h>
+# include <unistd.h>
+# include <limits.h>
 
 typedef struct s_type
 {
@@ -24,14 +26,16 @@ typedef struct s_type
 	int	hash;
 	int	space;
 	int	plus;
-}			t_type;
+}			t_flags;
 
-size_t	ft_strlen(const char *s);
 void	ft_bzero(void *s, size_t n);
-char	*ft_strdup(char *src);
-char	*ft_substr(char const *s, unsigned int start, size_t len);
-void	*ft_calloc(size_t nmemb, size_t size);
-char	**ft_nsplit(const char *s, char *sep);
-int		ft_printf(const char *s, ...);
+int		ft_printf(const char *str, ...);
+int		ft_putchar(char c);
+int		ft_putstr(char *s);
+int		ft_putnbr_base_hexa(long nb, char *base);
+int		ft_putnbr_decimal(long nb);
+int		ft_print_mem(void *p);
+int		ft_strlen(char *s);
+int		ft_see_format(const char *s, va_list var_args);
 
 #endif
