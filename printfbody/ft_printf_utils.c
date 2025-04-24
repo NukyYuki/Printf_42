691c978e65608ac9c97214d8efc590bc6b5b53e2
Manuel Maria Carreiro Couto <manmaria@c1r10s5.42porto.com>
2025-04-24 20:50:06 +0100

    disvster: rmved strlen, putstr and putchar files, added them to a utils file; added bzero

--- printfbody/ft_printf_utils.c
+++ printfbody/ft_printf_utils.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putchar.c                                       :+:      :+:    :+:   */
+/*   ft_printf_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mipinhei <mipinhei@student.42porto.com>    +#+  +:+       +#+        */
+/*   By: manmaria <manmaria@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/23 15:06:38 by mipinhei          #+#    #+#             */
-/*   Updated: 2025/04/23 17:22:55 by mipinhei         ###   ########.fr       */
+/*   Created: 2025/04/24 20:43:24 by manmaria          #+#    #+#             */
+/*   Updated: 2025/04/24 20:49:09 by manmaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,5 +18,38 @@ int	ft_putchar(char c)
 
 	count = 1;
 	write(1, &c, 1);
+	return (count);
+}
+
+void	ft_bzero(void *s, size_t n)
+{
+	unsigned char	*p_s;
+	size_t			i;
+
+	i = 0;
+	p_s = s;
+	while (i < n)
+		p_s[i++] = '\0';
+}
+
+int	ft_strlen(char *s)
+{
+	int	i;
+
+	if (!s)
+		return (0);
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+int	ft_putstr(char *s)
+{
+	int	count;
+
+	count = 0;
+	while (s[count])
+		count += ft_putchar(s[count]);
 	return (count);
 }
--- printfbody/libftprintf.h
+++ printfbody/libftprintf.h
@@ -6,7 +6,7 @@
 /*   By: mipinhei <mipinhei@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 13:22:25 by mipinhei          #+#    #+#             */
-/*   Updated: 2025/04/24 14:11:02 by mipinhei         ###   ########.fr       */
+/*   Updated: 2025/04/24 20:48:20 by manmaria         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,25 @@
 # include <unistd.h>
 # include <limits.h>
 
-int	ft_printf(const char *str, ...);
-int	ft_putchar(char c);
-int	ft_putstr(char *s);
-int	ft_putnbr_base_hexa(long, char *base);
-int	ft_putnbr_decimal(long nb);
-int	ft_print_mem(void *p);
-int	ft_strlen(char *s);
-int	ft_see_format(const char *s, va_list var_args);
+typedef struct s_type
+{
+	int	width;
+	int	minus;
+	int	zeros;
+	int	precision;
+	int	hash;
+	int	space;
+	int	plus;
+}			t_flags;
+
+void	ft_bzero(void *s, size_t n);
+int		ft_printf(const char *str, ...);
+int		ft_putchar(char c);
+int		ft_putstr(char *s);
+int		ft_putnbr_base_hexa(long nb, char *base);
+int		ft_putnbr_decimal(long nb);
+int		ft_print_mem(void *p);
+int		ft_strlen(char *s);
+int		ft_see_format(const char *s, va_list var_args);
 
 #endif
